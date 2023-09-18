ac800ce021a85163317acb5bc86824d0dc63eb9e
Mike <bhw8412@hotmail.com>
2023-09-18 19:45:06 +1000

    init proj

---
+++ app/src/main/java/com/hongwei/demo/coroutinesvisualiser/CoroutinesVisualiserApplication.kt
@@ -0,0 +1,9 @@
+package com.hongwei.demo.coroutinesvisualiser
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CoroutinesVisualiserApplication : Application() {
+
+}
